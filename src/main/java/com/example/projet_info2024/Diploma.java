97c54d258a76e59b8f6438c82fc5b8c8b7b74885
KelvinGavi <kelvin.gavillet@hes-so.ch>
2024-05-28 14:02:45 +0200

    Architecture class mère(ObjectScheme)/filles(Students/Diploma)

---
+++ src/main/java/com/example/projet_info2024/Diploma.java
@@ -0,0 +1,44 @@
+package com.example.projet_info2024;
+
+import javafx.scene.image.Image;
+
+public class Diploma extends ObjectScheme{
+
+    //Méthodes venant de la class abstract ObjectScheme
+
+    @Override
+    public void changeImage(Image image) {
+
+    }
+
+    @Override
+    public void changePosition(float positionX, float positionY) {
+
+    }
+
+    @Override
+    public void changeRadiusCommunication(float radius) {
+
+    }
+
+    @Override
+    public float[] getPosition() {
+        return new float[0];
+    }
+
+    @Override
+    public float getRadiusCommunication() {
+        return 0;
+    }
+
+    @Override
+    public Image getImage() {
+        return null;
+    }
+
+    @Override
+    public boolean isCommunication(ObjectScheme object) {
+        return false;
+    }
+
+}
