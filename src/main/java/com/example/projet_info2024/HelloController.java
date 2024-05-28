46296e162245b035e2b11d2105a1dc96617da155
jaurpell <jaures.pellaz@students.hevs.ch>
2024-05-28 13:04:14 +0200

    Initial commit

---
+++ src/main/java/com/example/projet_info2024/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.projet_info2024;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.projet_info2024 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.projet_info2024 to javafx.fxml;
+    exports com.example.projet_info2024;
+}
