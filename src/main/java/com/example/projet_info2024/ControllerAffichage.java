240d7a599dc413864feb08389b5ac18b3d32967c
jaurpell <jaures.pellaz@students.hevs.ch>
2024-06-04 13:33:38 +0200

    fxml 2

---
+++ src/main/java/com/example/projet_info2024/ControllerAffichage.java
@@ -0,0 +1,4 @@
+package com.example.projet_info2024;
+
+public class ControllerAffichage {
+}
---
+++ src/main/java/com/example/projet_info2024/MainController.java
@@ -0,0 +1,9 @@
+package com.example.projet_info2024;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+public class MainController extends Application {
+
+}
